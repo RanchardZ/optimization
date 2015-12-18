db458d08b7a44aa41d17f7d2f234326261c8ac23
RanchardZ <ranchardzheng@ruc.edu.cn>
2015-12-18 18:10:19 +0800

    move CODE_PATH and DATA_PATH to constant.py

---
+++ constant.py
@@ -0,0 +1,6 @@
+# some universal constants are set in here
+
+# CODE_PATH stored the address of this package
+CODE_PATH = '/home/lgp105b/zhenghe/zhCode/optimizaiton/'
+# DATA_PATH stored the address of data collected during experiment
+DATA_PATH = '/home/lgp105b/zhenghe/experimentData/'
